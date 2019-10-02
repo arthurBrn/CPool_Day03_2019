61ecb4e62dbf69ddbcb3b863103710f4c92ab66e
Baron <arthur.baron@epitech.eu>
2019-10-02 19:23:05 +0200

    02/10|19:22|piscineDay03| task 05 finished

---
+++ my_print_comb.c
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2019
+** my_print_comb
+** File description:
+** Task 05
+*/
+
+void my_putchar(char c);
+
+void display(int a)
+{
+    if (a < 7) {
+        my_putchar(',');
+        my_putchar(' ');
+    }
+}
+
+int my_print_comb(void)
+{
+    char a = 0;
+    char b = 1;
+    char c = 2;
+
+    while (a != 8) {
+        my_putchar(a + '0');
+        my_putchar(b + '0');
+        my_putchar(c + '0');
+        display(a);
+        if (c == 9 && b != 8) {
+            b = b + 1;
+            c = b;
+        }
+        if (c == 9 && b == 8) {
+            a++;
+            b = a + 1;
+            c = b + 1;
+        } else {
+            c++;
+        }
+    }
+    return (0);
+}
