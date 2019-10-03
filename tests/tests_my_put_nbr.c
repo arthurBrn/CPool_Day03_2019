24fd74ccbe983b309beec3b9c82fd785ecc92761
Baron <arthur.baron@epitech.eu>
2019-10-03 11:17:14 +0200

    02/10|11:16|piscineDay03| task 08 finished

---
+++ tests/tests_my_put_nbr.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** Testing
+** File description:
+** Task 08
+*/
+
+void my_put_nbr(int nb);
+
+int main(void)
+{
+    my_put_nbr(0);
+    my_put_nbr(1234);
+    my_put_nbr(-9876);
+    my_put_nbr(2147483647);
+    my_put_nbr(-2147483647);
+    return (0);
+}
